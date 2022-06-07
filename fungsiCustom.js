ce2ef6e3a21c4f6665f317b49571cefa3ce86a32
siddik suryana <sidiksuryana.ss@gmail.com>
2022-06-08 04:13:50 +0700

    Challenge 06 Done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
+
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,8 +20,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(
+    file1,
+    'utf-8',
+    (err, readData1) => {
+      if (err) {
+        return fnCallback(err);
+      };
+      fs.readFile(
+        file2,
+        'utf-8',
+        (err, readData2) => {
+          if (err) {
+            return fnCallback(err);
+          };
+          fs.readFile(
+            file3,
+            'utf-8',
+            (err, readData3) => {
+              let data1 = JSON.parse(readData1).message;
+              let data2 = JSON.parse(readData2)[0].message;
+              let data3 = JSON.parse(readData3)[0].data.message;
+              let result = `${data1} ${data2} ${data3}`;
 
+              fnCallback(
+                err,
+                result.split(' ').filter(element => element.toLowerCase() !=='halo')
+              )
+            }
+          )
+        }
+      )
+    }
+  )
+}
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
